2b5151437ba77158f588d778f68b0986c1321919
Marce23 <marcelitacamen2@gmail.com>
2021-11-16 14:11:41 -0500

    Modificaciones 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,7 @@ import { AuthService } from './services/auth.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
   usuario = {
     Email: '',
@@ -14,19 +15,25 @@ export class AppComponent {
 
   constructor(private authService: AuthService) {}
   Ingresar() {
-    console.log(this.usuario);
     const { Email, Contrasena } = this.usuario;
-    this.authService.login(Email, Contrasena).then((res) => {
-      console.log('se registro: ', res);
-    });
+    console.log("El Usuario es: " + this.usuario.Email);
+    console.log("La contraseña es: " + this.usuario.Contrasena);
+    // this.authService.login(Email, Contrasena).then((res) => {
+    //   console.log('se registro: ', res);
+    // });
   }
+
   IngresarConGoogle() {
     const { Email, Contrasena } = this.usuario;
+    console.log("El Usuario es: " + this.usuario.Email);
+    console.log("La contraseña es: " + this.usuario.Contrasena);
     this.authService.loginWinthGoogle(Email, Contrasena).then((res) => {
-      console.log('se registro: ', res);
+      console.log('Ingreso Por Google');
     });
   }
+
   Logout() {
     this.authService.logout();
+    console.log('Se Ha Finalizado La Sesion');
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
-
-
 export const environment = {
-  
-  production: true,
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyCMIg90l5cq8EICQxG-8MMbYfvUTEDVOfw',
+    authDomain: 'iniciarsesion-86a73.firebaseapp.com',
+    projectId: 'iniciarsesion-86a73',
+    storageBucket: 'iniciarsesion-86a73.appspot.com',
+    messagingSenderId: '746000848195',
+    appId: '1:746000848195:web:5f37337053f2896934cbf6',
+  },
 };
